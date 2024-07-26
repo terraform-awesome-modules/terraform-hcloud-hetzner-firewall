5ab14a95c7c9ac84691b7f374cff2c8cca4764d9
Armagan Karatosun <armagankaratosun@gmail.com>
2024-07-26 22:52:50 +0200

    fixed outputs

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
 output "firewall_id" {
-  value       = hcloud_firewall.firewall[count.index].id
+  value       = hcloud_firewall.firewall.id
   description = "The ID of the created firewall."
 }
 
 output "firewall_labels" {
-  value       = hcloud_firewall.firewall[count.index].labels
+  value       = hcloud_firewall.firewall.labels
   description = "The labels assigned to the firewall."
 }
 
 output "firewall_rules" {
-  value       = hcloud_firewall.firewall[count.index].rules
+  value       = hcloud_firewall.firewall.rules
   description = "The list of rules that are applied to the firewall."
 }
