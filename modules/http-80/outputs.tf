c5aca654404db85b03d3ca08f3297eb56a25df34
Armagan Karatosun <armagankaratosun@gmail.com>
2024-07-27 15:10:53 +0200

    added outputs for submodules, now only [0]

--- modules/http-80/outputs.tf
+++ modules/http-80/outputs.tf
@@ -1,15 +1,15 @@
 output "firewall_ids" {
-  value = module.firewall.firewall_ids
+  value = module.firewall.firewall_ids[0]
   description = "A map of all created firewall IDs, keyed by their index."
 }
 
 output "firewall_labels" {
-  value = module.firewall.firewall_labels
+  value = module.firewall.firewall_labels[0]
   description = "A map of labels assigned to each firewall, keyed by their index."
 }
 
 output "firewall_rules" {
-  value = module.firewall.firewall_rules
+  value = module.firewall.firewall_rules[0]
   description = "The list of rules that are applied to each firewall, indexed by firewall."
 }
 
